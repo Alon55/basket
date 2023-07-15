7458feaee230e4d6199181c51cc5a8d0926516c2
alon55 <alonzager5@gmail.com>
2023-07-15 17:11:06 +0300

    kantox

--- app/components/product.js
+++ app/components/product.js
@@ -4,9 +4,23 @@ import { action } from '@ember/object';
 
 export default class ProductComponent extends Component {
   @tracked quantity = 0;
+  @tracked total_price = 0;
 
   @action
-  addToCart() {
-    this.quantity++;
+  addToCart(e) {
+    if (e.target.id === 'GR1') {
+      this.quantity += 2;
+      this.total_price += 3.11;
+    } // tea
+    if (e.target.id === 'SR1') {
+      this.quantity++;
+      if (this.quantity >= 3) { this.total_price += 4.50 } else { this.total_price += 5.00 }
+    } // strawberries
+    if (e.target.id === 'CF1') {
+      this.quantity++;
+      if (this.quantity >= 3) { this.total_price += 11.23 / 3 * 2 } else { this.total_price += 11.23 }
+    } // coffee
+
   }
+
 }
